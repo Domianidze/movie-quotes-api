206d673632e03e596bd8eb1ff272f34a1545e3ae
Domianidze <sdomianidze123@gmail.com>
2022-07-22 21:27:46 +0400

    feat: set up mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import express, { Express, Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
 
 const server: Express = express()
 
 server.use(bodyParser.json())
 
 server.use((_: Request, res: Response) => {
-  res.json('Hello World!')
+  res.json({
+    message: 'Hello World!',
+  })
 })
 
-server.listen(3000)
+const startServer = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://sdomianidze:4GMX680AWm6LMtQS@cluster0.8bito.mongodb.net/main?retryWrites=true&w=majority'
+    )
+    server.listen(8080)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+startServer()
