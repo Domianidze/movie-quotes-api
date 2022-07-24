a7c201b3413c3aeecaa6a641c3c2bc6979fdf554
Domianidze <sdomianidze123@gmail.com>
2022-07-24 15:33:34 +0400

    feat: set up swagger

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express, { Express, Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
+import SwaggerUI from 'swagger-ui-express'
 import 'dotenv/config'
 
+import { swaggerMiddleware } from 'middleware'
 import { getMongoUrl } from 'helpers'
 
 const server: Express = express()
 
 server.use(bodyParser.json())
+
+server.use('/api-docs', SwaggerUI.serve, swaggerMiddleware())
 
 server.use((_: Request, res: Response) => {
   res.json({
@@ -18,7 +22,7 @@ server.use((_: Request, res: Response) => {
 const startServer = async () => {
   try {
     await mongoose.connect(getMongoUrl())
-    server.listen(process.env.SEVER_PORT)
+    server.listen(process.env.SERVER_PORT)
   } catch (err) {
     console.error(err)
   }
