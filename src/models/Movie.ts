0c0c96fbe166c107131ba1374bd5139e867d3b9e
Domianidze <sdomianidze123@gmail.com>
2022-08-01 13:54:43 +0400

    feat: add movie model

---
+++ src/models/Movie.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+const movieSchema = new Schema({
+  name: {
+    type: Object,
+    required: true,
+  },
+  tags: {
+    type: Array,
+    required: true,
+  },
+  director: {
+    type: Object,
+    required: true,
+  },
+  description: {
+    type: Object,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+})
+
+const Movie = mongoose.model('Movie', movieSchema)
+
+export default Movie
