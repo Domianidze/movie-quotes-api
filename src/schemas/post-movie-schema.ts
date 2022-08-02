06106ba13ef5d4926605ac4af7db71d567445249
Domianidze <sdomianidze123@gmail.com>
2022-08-02 10:24:51 +0400

    refactor: improve validation

--- src/schemas/post-movie-schema.ts
+++ src/schemas/post-movie-schema.ts
@@ -1,11 +1,23 @@
 import Joi from 'joi'
 
 export default Joi.object({
-  nameEn: Joi.string().required(),
-  nameGe: Joi.string().required(),
+  nameEn: Joi.string()
+    .regex(/^[a-zA-Z0-9 .,!?'„“():/]*$/)
+    .required(),
+  nameGe: Joi.string()
+    .regex(/^[ა-ჰ0-9 .,!?'„“():/]*$/)
+    .required(),
   tags: Joi.string().required(),
-  directorEn: Joi.string().required(),
-  directorGe: Joi.string().required(),
-  descriptionEn: Joi.string().required(),
-  descriptionGe: Joi.string().required(),
+  directorEn: Joi.string()
+    .regex(/^[a-zA-Z0-9 .,!?'„“():/]*$/)
+    .required(),
+  directorGe: Joi.string()
+    .regex(/^[ა-ჰ0-9 .,!?'„“():/]*$/)
+    .required(),
+  descriptionEn: Joi.string()
+    .regex(/^[a-zA-Z0-9 .,!?'„“():/]*$/)
+    .required(),
+  descriptionGe: Joi.string()
+    .regex(/^[ა-ჰ0-9 .,!?'„“():/]*$/)
+    .required(),
 })
--- src/schemas/put-movie-schema.ts
+++ src/schemas/put-movie-schema.ts
@@ -2,11 +2,11 @@ import Joi from 'joi'
 
 export default Joi.object({
   id: Joi.any(),
-  nameEn: Joi.string(),
-  nameGe: Joi.string(),
+  nameEn: Joi.string().regex(/^[a-zA-Z0-9 .,!?'„“():/]*$/),
+  nameGe: Joi.string().regex(/^[ა-ჰ0-9 .,!?'„“():/]*$/),
   tags: Joi.string(),
-  directorEn: Joi.string(),
-  directorGe: Joi.string(),
-  descriptionEn: Joi.string(),
-  descriptionGe: Joi.string(),
+  directorEn: Joi.string().regex(/^[a-zA-Z0-9 .,!?'„“():/]*$/),
+  directorGe: Joi.string().regex(/^[ა-ჰ0-9 .,!?'„“():/]*$/),
+  descriptionEn: Joi.string().regex(/^[a-zA-Z0-9 .,!?'„“():/]*$/),
+  descriptionGe: Joi.string().regex(/^[ა-ჰ0-9 .,!?'„“():/]*$/),
 }).min(2)
