a79c0ccdcfef6cbe9029819ea89f18af60b0f768
Domianidze <sdomianidze123@gmail.com>
2022-08-11 14:59:51 +0400

    feat: add notification model

---
+++ src/models/Notification.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+const notificationSchema = new Schema({
+  notification: {
+    type: String,
+    enum: ['commented', 'reacted'],
+    required: true,
+  },
+  read: { type: Boolean, default: false },
+  date: { type: Date, default: Date.now() },
+  to: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  by: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+})
+
+const Notification = mongoose.model('Notification', notificationSchema)
+
+export default Notification
